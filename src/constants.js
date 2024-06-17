b9e4892aa8b8622d0ff686c7b90c23064b65a109
SantiagoFigli <107778558+SantiagoFigli@users.noreply.github.com>
2024-06-17 14:24:41 -0300

    upload files

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in Java and Javascript these days.
+    I've made a couple of projects   in that language. I also like  and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/SantiagoFigli" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Computer Science degree from the National Technological University of Mar del Plata. I hung it on the wall because I'm proud of it. It was a very comprehensive program, and I am happy to know that it is only the beginning of this exciting journey.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch some movies and series.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@derekbanas" target="_blank">Derek Banas</a>, <a href="https://www.youtube.com/@programmingwithmosh" target="_blank">Programming with Mosh</a>,
+  <a href="https://www.youtube.com/@andresguzf" target="_blank">Andres Guzman Dev</a> (Java+Spring specialist) and <a href="https://www.youtube.com/@midulive" target="_blank">midulive</a>!`,
+  bed: `This is my bed, where I rest when I'm exhausted from working on the computer. I also use it to calm down and refocus when I'm mentally overloaded.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/SantiagoFigli/Resume/blob/main/CV%20Santiago%20Figliuolo.pdf" target="_blank">Check it out?</a>
+  Contact me at santiagofigliuolo@protonmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. There is even one in English (I also speak Italian btw).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit SantiagoFigli's portfolio, just close the tab.`,
+}
